4d65321b4510bfe1105c5048f4fed49750f51630
thanhdc <blackwolf196@gmail.com>
2026-05-17 16:52:38 +0700

    update readme.md, update file name by rule

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-import ErrorBoundary from '@/components/ErrorBoundary';
-import withSuspense from '@/hoc/withSuspense';
-import MainLayout from '@/layouts/MainLayout';
+import ErrorBoundary from '@/components/error-boundary';
+import withSuspense from '@/hoc/with-suspense';
+import MainLayout from '@/layouts/main-layout';
 
-const Home = withSuspense(lazy(() => import('@/views/Home')));
-const About = withSuspense(lazy(() => import('@/views/About')));
+const Home = withSuspense(lazy(() => import('@/views/home')));
+const About = withSuspense(lazy(() => import('@/views/about')));
 const NotFound = withSuspense(
   lazy(() => import('../views/not-found/not-found')),
 );
@@ -14,22 +14,22 @@ const NotFound = withSuspense(
 export const routes = [
   {
     path: '/',
-    element: <MainLayout />,
-    errorElement: <ErrorBoundary />,
+    element: <MainLayout/>,
+    errorElement: <ErrorBoundary/>,
     children: [
       {
         index: true,
-        element: <Home />,
+        element: <Home/>,
       },
       {
         path: 'about',
-        element: <About />,
+        element: <About/>,
       },
     ],
   },
   {
     path: '*',
-    element: <NotFound />,
+    element: <NotFound/>,
   },
 ];
 
